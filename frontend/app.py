27021f2d7a6107c0fe3b0edf4a4a116b6deefc43
bobcastaldeli <castaldelliroberto@gmail.com>
2023-03-03 17:15:45 -0300

    fix: frontend widgets

--- frontend/app.py
+++ frontend/app.py
@@ -2,14 +2,33 @@
 
 
 import requests
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
 
 st.title("App Medical Docs Classifier App")
-st.write("This app predicts the type of cancer based on a medical document")
+st.subheader(
+    "This app predicts the type of cancer based on a medical document"
+)
+st.write(
+    """
+    For Biomedical text document classification, abstract and full papers
+    available and used. This dataset focused on long research paper whose
+    page size more than 6 pages. Dataset includes cancer documents to be
+    classified into 3 categories like 'Thyroid_Cancer', 'Colon_Cancer', 'Lung_Cancer'.
+    """
+)
 
 
-input_text = st.text_input("Enter your text here")
+col1, col2, col3 = st.columns(3)
+col1.metric("Size of the dataset", "7569")
+col2.metric("Train size", "6055", "80%")
+col3.metric("Test size", "1514", "20%")
+
+
+input_text = st.text_area("Enter your text here")
+
 
 if st.button("Predict"):
     data = {"text": input_text}
